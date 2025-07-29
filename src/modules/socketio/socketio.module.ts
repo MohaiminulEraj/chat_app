fc87e11af02a556be8711ef56922409997e9ddee
MohaiminulEraj <eraj471@gmail.com>
2025-07-30 00:20:24 +0600

    auth connection issue fixing

--- src/modules/socketio/socketio.module.ts
+++ src/modules/socketio/socketio.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
+import { jwtConfig } from 'src/config/jwt.config'
 import { ConversationModule } from '../conversation/conversation.module'
 import { FriendshipModule } from '../friendship/friendship.module'
 import { GroupModule } from '../group/group.module'
@@ -9,10 +10,7 @@ import { SocketIOService } from './socketio.service'
 
 @Module({
     imports: [
-        JwtModule.register({
-            secret: process.env.JWT_SECRET || 'your-secret-key',
-            signOptions: { expiresIn: '24h' }
-        }),
+        JwtModule.registerAsync(jwtConfig),
         UserModule,
         ConversationModule,
         FriendshipModule,
