b84d3e8fa04225855dec96bfd1ca53073adb8ad1
MohaiminulEraj <eraj471@gmail.com>
2025-08-24 23:53:21 +0600

    groupId made string

--- src/modules/room/dto/create-room.dto.ts
+++ src/modules/room/dto/create-room.dto.ts
@@ -17,7 +17,7 @@ export class CreateRoomDto {
         description: 'The UUID of the group this room belongs to',
         example: '123e4567-e89b-12d3-a456-426614174000'
     })
-    @IsUUID()
+    @IsString()
     groupId: string
 
     @ApiProperty({
